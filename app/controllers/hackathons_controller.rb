2aa8e2a6367a4d2e90250a95db152c2bde14488d
sezeristif <sezeristif@gmail.com>
2020-12-26 17:17:50 +0300

    add show action for hackathons controller

--- app/controllers/hackathons_controller.rb
+++ app/controllers/hackathons_controller.rb
@@ -3,4 +3,9 @@ class HackathonsController < ApplicationController
     hackathons = HackathonSerializer.new(Hackathon.all).serializable_hash.to_json
     render json: hackathons
   end
+
+  def show
+    hackathon = HackathonSerializer.new(Hackathon.find_by(id: params[:id])).serializable_hash.to_json
+    render json: hackathon
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'hackathons', to: 'hackathons#index'
+  resources :hackathons, only: [:index, :show]
 end
